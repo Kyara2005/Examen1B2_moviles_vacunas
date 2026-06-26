6a014c27b8b08b72f3db5b89472542a8a88d6216
Kyara2005 <kyara.altamirano@epn.edu.ec>
2026-06-26 00:25:47 -0500

    icono

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,12 +1,12 @@
 class AppConstants {
   static const String supabaseUrl = String.fromEnvironment(
     'SUPABASE_URL',
-    defaultValue: 'https://TU-PROYECTO.supabase.co',
+    defaultValue: 'https://haubjiclkskymrpuuqph.supabase.co',
   );
 
   static const String supabaseAnonKey = String.fromEnvironment(
     'SUPABASE_ANON_KEY',
-    defaultValue: 'TU-ANON-KEY',
+    defaultValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhdWJqaWNsa3NreW1ycHV1cXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NzgxMTcsImV4cCI6MjA5NDM1NDExN30.A8CqXytC6X3pjn7eSoqf6vGtpoTqSXPxb_9i5GtywUk',
   );
 
   static const String claveInicial = 'Ecuador2026';
